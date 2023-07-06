e755145f883342a6dfc629abdc377b857797d8dc
tarekul42 <tarekulrifat205@gmail.com>
2023-07-06 16:50:11 +0600

    spinner, displays 10 phones

--- js/app.js
+++ js/app.js
@@ -40,11 +40,12 @@ const displayPhones = (phones, dataLimit) =>{
     }
     // display all phones
     phones.forEach(phone =>{
+        console.log(phone.image);
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
-        phonesContainer.innerHTML = `
+        phoneDiv.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.images}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
