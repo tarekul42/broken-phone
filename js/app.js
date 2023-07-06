958e9df747e61e5f05723a5e8a4e0d1925798a0d
tarekul42 <tarekulrifat205@gmail.com>
2023-07-06 16:30:10 +0600

    search field done

---
+++ js/app.js
@@ -0,0 +1,119 @@
+/*
+*  1)Search button not working
+*  2)after typing something on the search textbox, you should be able to press enter to get search result
+*  3)spinner is always running. should be displayed at the time of data loading
+*  4)If you search for 'iphone', check the api, you should get a lot of data but the UI displays only one phone
+*  5)Search is not clearing previous results
+*  6)picture of the phones are not showing
+*  7)Show All button should be hidden after all data is loaded
+*  8)Load phone details doesn't display anything
+*  9)Phone details are added on the modal but this is not getting displayed. why?
+*  10)Storage on Modal isn't getting displayed properly
+*/
+
+const loadPhones = async(searchText, dataLimit) =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data, dataLimit);
+}
+
+const displayPhones = (phones, dataLimit) =>{
+    const phonesContainer = document.getElementById('phones-container');
+    // phonesContainer.textContent = '';
+    // display 10 phones only 
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-hidden');
+    }
+    // display no phones found
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+    // display all phones
+    phones.forEach(phone =>{
+        const phoneDiv  = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phonesContainer.innerHTML = `
+        <div class="card p-4">
+            <img src="${phone.images}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+                
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+    });
+    // stop spinner or loader
+    toggleSpinner(false);
+}
+
+const processSearch = (dataLimit) =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
+// handle search button click
+document.getElementById('btn-search').addEventListener('click', function(){
+    // start loader
+    processSearch(10);
+    toggleSpinner(true);
+})
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
+});
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+
+// not the best way to load show All
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    processSearch();
+})
+
+const loadPhoneDetails = async id =>{
+    const url =`www.openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-details');
+    console.log(phone.mainFeatures.sensors[0]);
+    phoneDetails.innerHTML = `
+        <p>Release Date: ${phone.releaseDate}</p>
+        <p>Storage: ${phone.mainFeatures}</p>
+        <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
+        <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
+    `
+}
+
+// loadPhones('apple');
